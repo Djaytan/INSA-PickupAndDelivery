50aea3c07d0b13fb9674345899621f7fd4a7ea38
Voltariuss <loic.duboistermoz@gmail.com>
2021-03-17 17:20:38 +0100

    feat: adds XML handlers for map and requests description XML files

---
+++ src/main/java/fr/insa/lyon/ifa1/xml/XMLMapHandler.java
@@ -0,0 +1,10 @@
+package fr.insa.lyon.ifa1.xml;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * This class represents the XML handler for map description file of the city.
+ *
+ * @author IFA1
+ */
+public class XMLMapHandler extends DefaultHandler {}
---
+++ src/main/java/fr/insa/lyon/ifa1/xml/XMLRequestsHandler.java
@@ -0,0 +1,10 @@
+package fr.insa.lyon.ifa1.xml;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * This class represents the XML handler for requests description file.
+ *
+ * @author IFA1
+ */
+public class XMLRequestsHandler extends DefaultHandler {}

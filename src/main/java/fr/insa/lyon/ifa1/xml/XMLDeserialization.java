9e4c348ed0a6cfb939a53f51e9c7d7bcb5143926
Voltariuss <loic.duboistermoz@gmail.com>
2021-03-17 17:42:24 +0100

    feat: finizalizes core deserialization method

--- src/main/java/fr/insa/lyon/ifa1/xml/XMLDeserialization.java
+++ src/main/java/fr/insa/lyon/ifa1/xml/XMLDeserialization.java
@@ -1,5 +1,7 @@
 package fr.insa.lyon.ifa1.xml;
 
+import java.io.File;
+import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -20,12 +22,14 @@ public class XMLDeserialization {
    * @param handler The XML handler to use for deserialization.
    * @throws SAXException If general SAX errors or warnings are raised.
    * @throws ParserConfigurationException If a serious configuration error is detected.
+   * @throws IOException If a problem occur during the reading process of the XML file.
    */
   public void deserialize(String fileName, DefaultHandler handler)
-      throws SAXException, ParserConfigurationException {
+      throws SAXException, ParserConfigurationException, IOException {
+    File inputFile = new File(fileName);
     SAXParserFactory spf = SAXParserFactory.newInstance();
     SAXParser saxParser = spf.newSAXParser();
-    // TODO: complete
+    saxParser.parse(inputFile, handler);
   }
 
   /**
@@ -34,8 +38,10 @@ public class XMLDeserialization {
    * @param fileName The name of the targeted XML file of map description.
    * @throws SAXException If general SAX errors or warnings are raised.
    * @throws ParserConfigurationException If a serious configuration error is detected.
+   * @throws IOException If a problem occur during the reading process of the XML file.
    */
-  public void deserializeMap(String fileName) throws SAXException, ParserConfigurationException {
+  public void deserializeMap(String fileName)
+      throws SAXException, ParserConfigurationException, IOException {
     XMLMapHandler xmlMapHandler = new XMLMapHandler();
     deserialize(fileName, xmlMapHandler);
   }
@@ -46,9 +52,10 @@ public class XMLDeserialization {
    * @param fileName The name of the targeted XML file of requests description.
    * @throws SAXException If general SAX errors or warnings are raised.
    * @throws ParserConfigurationException If a serious configuration error is detected.
+   * @throws IOException If a problem occur during the reading process of the XML file.
    */
   public void deserializeRequests(String fileName)
-      throws SAXException, ParserConfigurationException {
+      throws SAXException, ParserConfigurationException, IOException {
     XMLRequestsHandler xmlRequestsHandler = new XMLRequestsHandler();
     deserialize(fileName, xmlRequestsHandler);
   }

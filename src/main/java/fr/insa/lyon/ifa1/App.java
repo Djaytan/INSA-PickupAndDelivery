0cc4b1aa8326e93a0b67eab777ce9145fa8edc39
Voltariuss <loic.duboistermoz@gmail.com>
2021-03-19 15:11:57 +0100

    test: setup XML deserialization test for map description

--- src/main/java/fr/insa/lyon/ifa1/App.java
+++ src/main/java/fr/insa/lyon/ifa1/App.java
@@ -1,8 +1,30 @@
 package fr.insa.lyon.ifa1;
 
+import fr.insa.lyon.ifa1.cache.GeoMapRegistry;
+import fr.insa.lyon.ifa1.xml.XMLDeserialization;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
 /** Hello world! */
 public class App {
+
+  private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+  private static final String TEST_MAP_FILE = "smallMap.xml";
+
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    try {
+      XMLDeserialization.deserializeMap(TEST_MAP_FILE);
+      LOGGER.info(GeoMapRegistry.getGeoMap().toString());
+    } catch (SAXException e) {
+      LOGGER.log(Level.SEVERE, "Error during XML file content reading", e);
+    } catch (IOException e) {
+      LOGGER.log(Level.SEVERE, "Error during XML file manipulation", e);
+    } catch (ParserConfigurationException e) {
+      LOGGER.log(Level.SEVERE, "Something went wrong in XML parser configuration", e);
+    }
   }
 }

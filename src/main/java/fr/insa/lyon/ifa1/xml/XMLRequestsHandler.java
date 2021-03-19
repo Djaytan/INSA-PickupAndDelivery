8e97ec27391af8ee9c284e3f46e60a0266e7be1a
Voltariuss <loic.duboistermoz@gmail.com>
2021-03-19 15:55:05 +0100

    feat: implements XML request handler

--- src/main/java/fr/insa/lyon/ifa1/xml/XMLRequestsHandler.java
+++ src/main/java/fr/insa/lyon/ifa1/xml/XMLRequestsHandler.java
@@ -1,5 +1,13 @@
 package fr.insa.lyon.ifa1.xml;
 
+import fr.insa.lyon.ifa1.cache.GeoMapRegistry;
+import fr.insa.lyon.ifa1.cache.PlanningRequestRegistry;
+import fr.insa.lyon.ifa1.models.map.Intersection;
+import fr.insa.lyon.ifa1.models.request.Depot;
+import fr.insa.lyon.ifa1.models.request.Depot.Time;
+import fr.insa.lyon.ifa1.models.request.DurationPassagePoint;
+import fr.insa.lyon.ifa1.models.request.Request;
+import java.util.jar.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
@@ -10,20 +18,48 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 public class XMLRequestsHandler extends DefaultHandler {
 
+  public static final String PICKUP_TYPE = "pickup";
+  public static final String DELIVERY_TYPE = "delivery";
+
   @Override
   public void startElement(String uri, String localName, String qName, Attributes attributes)
       throws SAXException {
     switch (qName) {
       case "planningRequest":
         {
+          /* Do nothing */
           break;
         }
       case "depot":
         {
+          String intersectionId = attributes.getValue("address");
+          String departureTimeStr = attributes.getValue("departureTime");
+          String[] departureTimeSplited = departureTimeStr.split(":");
+          int hours = Integer.parseInt(departureTimeSplited[0]);
+          int minutes = Integer.parseInt(departureTimeSplited[1]);
+          int seconds = Integer.parseInt(departureTimeSplited[2]);
+          Time departureTime = new Time(hours, minutes, seconds);
+          Intersection intersection = GeoMapRegistry.getGeoMap().getIntersection(intersectionId);
+          Depot depot = new Depot(intersection, departureTime);
+          PlanningRequestRegistry.createPlanningRequest(depot);
           break;
         }
       case "request":
         {
+          String pickupIntersectionId = attributes.getValue("pickupAddress");
+          String deliveryIntersectionId = attributes.getValue("deliveryAddress");
+          int pickupDuration = Integer.parseInt(attributes.getValue("pickupDuration"));
+          int deliveryDuration = Integer.parseInt(attributes.getValue("deliveryDuration"));
+          Intersection pickupIntersection =
+              GeoMapRegistry.getGeoMap().getIntersection(pickupIntersectionId);
+          Intersection deliveryIntersection =
+              GeoMapRegistry.getGeoMap().getIntersection(deliveryIntersectionId);
+          DurationPassagePoint pickupPassagePoint =
+              new DurationPassagePoint(pickupIntersection, pickupDuration, PICKUP_TYPE);
+          DurationPassagePoint delivertPassagePoint =
+              new DurationPassagePoint(deliveryIntersection, deliveryDuration, DELIVERY_TYPE);
+          Request request = new Request(pickupPassagePoint, delivertPassagePoint);
+          PlanningRequestRegistry.getPlanningRequest().addRequest(request);
           break;
         }
       default:

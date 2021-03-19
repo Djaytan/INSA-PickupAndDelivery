82999f4df711e3666f90924fe129e6eaa9f69b39
Voltariuss <loic.duboistermoz@gmail.com>
2021-03-19 14:25:35 +0100

    style: applies google-java-format

--- src/main/java/fr/insa/lyon/ifa1/xml/XMLMapHandler.java
+++ src/main/java/fr/insa/lyon/ifa1/xml/XMLMapHandler.java
@@ -1,11 +1,10 @@
 package fr.insa.lyon.ifa1.xml;
 
+import fr.insa.lyon.ifa1.cache.GeoMapRegistry;
+import fr.insa.lyon.ifa1.models.map.Intersection;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-
-import fr.insa.lyon.ifa1.cache.GeoMapRegistry;
-import fr.insa.lyon.ifa1.models.map.Intersection;
 
 /**
  * This class represents the XML handler for map description file of the city.
@@ -14,28 +13,31 @@ import fr.insa.lyon.ifa1.models.map.Intersection;
  */
 public class XMLMapHandler extends DefaultHandler {
 
-    @Override
-    public void startElement (String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        switch (qName) {
-            case "map": {
-                /* Do nothing */
-                break;
-            }
-            case "intersection": {
-                String id = attributes.getValue("id");
-                double latitude = Double.parseDouble(attributes.getValue("latitude"));
-                double longitude = Double.parseDouble(attributes.getValue("longitude"));
-                Intersection intersection = new Intersection(id, latitude, longitude);
-                GeoMapRegistry.getGeoMap().addIntersection(intersection);
-                break;
-            }
-            case "segment": {
-                break;
-            }
-            default: {
-
-                break;
-            }
+  @Override
+  public void startElement(String uri, String localName, String qName, Attributes attributes)
+      throws SAXException {
+    switch (qName) {
+      case "map":
+        {
+          /* Do nothing */
+          break;
+        }
+      case "intersection":
+        {
+          String id = attributes.getValue("id");
+          double latitude = Double.parseDouble(attributes.getValue("latitude"));
+          double longitude = Double.parseDouble(attributes.getValue("longitude"));
+          Intersection intersection = new Intersection(id, latitude, longitude);
+          GeoMapRegistry.getGeoMap().addIntersection(intersection);
+          break;
+        }
+      case "segment":
+        {
+          break;
+        }
+      default:
+        {
         }
     }
+  }
 }

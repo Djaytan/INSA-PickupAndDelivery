60026545451e2771f785db8618d753cc5fbe9961
Kesly Gassant <kesly.gassant@insa-lyon.fr>
2021-03-24 14:49:10 +0100

    hamiltonian algorithm using backtracking

--- src/main/java/fr/insa/lyon/ifa1/algo/OptimalHamiltonianCircuit.java
+++ src/main/java/fr/insa/lyon/ifa1/algo/OptimalHamiltonianCircuit.java
@@ -4,36 +4,69 @@ import fr.insa.lyon.ifa1.models.map.GeoMap;
 import fr.insa.lyon.ifa1.models.request.PassagePoint;
 import fr.insa.lyon.ifa1.models.request.PlanningRequest;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class OptimalHamiltonianCircuit implements FindShortestHamiltonianCircuit {
+
+    private double optimalLength;
+    private List<String> optimalRoute;
+
     @Override
     public List<PassagePoint> solve(GeoMap gm, Map<String, Map<String, FindShortestRoutes.Route>> routes, PlanningRequest pr) {
 
+        this.optimalLength = Double.POSITIVE_INFINITY;
+        this.optimalRoute = null;
+
         Set<String> ppNotVisited = new HashSet<>();
 
-        for (PassagePoint pp : pr.getPassagePoints()) {
+        PassagePoint[] passagePoints = pr.getPassagePoints();
+        // init
+        for (PassagePoint pp : passagePoints) {
             ppNotVisited.add(pp.getAddress().getId());
         }
-        ppNotVisited.remove(pr.getDepot().getAddress().getId());
 
+        String idDepot = pr.getDepot().getAddress().getId();
+        ppNotVisited.remove(idDepot);
 
+        List<String> ppVisited = new ArrayList<>();
+        ppVisited.add(idDepot);
+
+        visitePoint(idDepot, ppVisited, ppNotVisited, routes, 0d, idDepot);
+
+        return this.optimalRoute.stream().map(
+                p -> Arrays.stream(passagePoints)
+                        .filter(pp -> pp.getAddress().getId().equals(p))
+                        .findFirst()
+                        .get()
+        ).toList();
     }
 
-    private void visitePoint(Set<String> ppNotVisited, Map<String, Map<String, FindShortestRoutes.Route>> routes, Double lengthCircuit ) {
+    private void visitePoint(String currentPoint, List<String> ppVisited, Set<String> ppNotVisited, Map<String, Map<String, FindShortestRoutes.Route>> routes, double lengthCircuit, String idDepot) {
 
-        if(ppNotVisited.isEmpty()){
+        if (ppNotVisited.isEmpty()) {
             // Calcule la valeur definif  = lengthCircuit + distance vers depot
+            lengthCircuit += routes.get(currentPoint).get(idDepot).getLength();
+
+            if (lengthCircuit < optimalLength) {
+                optimalLength = lengthCircuit;
+
+                List<String> clonePpVisited = new ArrayList<>(ppVisited);
+                clonePpVisited.add(idDepot);
+                optimalRoute = clonePpVisited;
+            }
             // finish
             // verifier si c'est chemin le plus court
         }
 
-        for(String ppId : ppNotVisited) {
+        Set<String> clonePpNotVisited = new HashSet<>(ppNotVisited);
 
+        for (String ppId : clonePpNotVisited) {
+            ppNotVisited.remove(ppId);
+            ppVisited.add(ppId);
+            visitePoint(ppId, ppVisited, ppNotVisited, routes, lengthCircuit + routes.get(currentPoint).get(ppId).getLength(), idDepot);
 
+            ppNotVisited.add(ppId);
+            ppVisited.remove(ppId);
         }
     }
 }

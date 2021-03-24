1d2001709476ee114bc9e1e8969a4f79f41c7ac3
Niels de Barbanson <ndebarbans@insa-lyon.fr>
2021-03-24 16:54:11 +0100

    ajout TU cycle Hamiltonien

--- src/test/java/fr/insa/lyon/ifa1/algo/OptimalHamiltonianCircuitTest.java
+++ src/test/java/fr/insa/lyon/ifa1/algo/OptimalHamiltonianCircuitTest.java
@@ -37,7 +37,7 @@ public class OptimalHamiltonianCircuitTest {
         Map<String, Segment> destDeG = new HashMap<>();
 
         destDeDepot.put("A", new Segment(2, "rue", intersections.get("Depot"), intersections.get("A")));
-        destDeDepot.put("G", new Segment(2, "rue", intersections.get("DepotG"), intersections.get("G")));
+        destDeDepot.put("G", new Segment(2, "rue", intersections.get("Depot"), intersections.get("G")));
         destDeDepot.put("D", new Segment(20, "rue", intersections.get("Depot"), intersections.get("D")));
         destDeA.put("Depot", new Segment(2, "rue", intersections.get("A"), intersections.get("Depot")));
         destDeA.put("B", new Segment(3, "rue", intersections.get("A"), intersections.get("B")));
@@ -77,17 +77,17 @@ public class OptimalHamiltonianCircuitTest {
 
         GeoMap gM = new GeoMap(intersections, segments);
 
-        Depot depot = new Depot(new Intersection("depot", 0, 0), new Depot.Time(7, 0, 0));
+        Depot depot = new Depot(new Intersection("Depot", 0, 0), new Depot.Time(7, 0, 0));
 
-        DurationPassagePoint P1 = new DurationPassagePoint(intersections.get("A"),2,"pickup");
-        DurationPassagePoint P2 = new DurationPassagePoint(intersections.get("B"),2,"pickup");
-        DurationPassagePoint P3 = new DurationPassagePoint(intersections.get("C"),2,"pickup");
-        DurationPassagePoint P4 = new DurationPassagePoint(intersections.get("D"),2,"pickup");
+        DurationPassagePoint P1 = new DurationPassagePoint(intersections.get("A"),2,PassagePointType.PICKUP);
+        DurationPassagePoint P2 = new DurationPassagePoint(intersections.get("B"),2,PassagePointType.PICKUP);
+        DurationPassagePoint P3 = new DurationPassagePoint(intersections.get("C"),2,PassagePointType.PICKUP);
+        DurationPassagePoint P4 = new DurationPassagePoint(intersections.get("D"),2,PassagePointType.PICKUP);
 
-        DurationPassagePoint D1 = new DurationPassagePoint(intersections.get("F"),2,"delivery");
-        DurationPassagePoint D2 = new DurationPassagePoint(intersections.get("F"),2,"delivery");
-        DurationPassagePoint D3 = new DurationPassagePoint(intersections.get("E"),2,"delivery");
-        DurationPassagePoint D4 = new DurationPassagePoint(intersections.get("G"),2,"delivery");
+        DurationPassagePoint D1 = new DurationPassagePoint(intersections.get("F"),2,PassagePointType.DELIVERY);
+        DurationPassagePoint D2 = new DurationPassagePoint(intersections.get("F"),2,PassagePointType.DELIVERY);
+        DurationPassagePoint D3 = new DurationPassagePoint(intersections.get("E"),2,PassagePointType.DELIVERY);
+        DurationPassagePoint D4 = new DurationPassagePoint(intersections.get("G"),2,PassagePointType.DELIVERY);
 
         Request[] requests = new Request[]{
                 new Request(P1,D1),
@@ -99,58 +99,73 @@ public class OptimalHamiltonianCircuitTest {
         PlanningRequest pr = new PlanningRequest(requests,depot);
 
 
-        Map<String, Map<String, FindShortestRoutes.Route>> routes = new HashMap<>();
-        Map<String, FindShortestRoutes.Route> routeDeDepot = new HashMap<>();
-        Map<String, FindShortestRoutes.Route> routeDeA = new HashMap<>();
-        Map<String, FindShortestRoutes.Route> routeDeB = new HashMap<>();
-        Map<String, FindShortestRoutes.Route> routeDeC = new HashMap<>();
-        Map<String, FindShortestRoutes.Route> routeDeD = new HashMap<>();
-        Map<String, FindShortestRoutes.Route> routeDeE = new HashMap<>();
-        Map<String, FindShortestRoutes.Route> routeDeF = new HashMap<>();
-        Map<String, FindShortestRoutes.Route> routeDeG = new HashMap<>();
+//        Map<String, Map<String, FindShortestRoutes.Route>> routes = new HashMap<>();
+//        Map<String, FindShortestRoutes.Route> routeDeDepot = new HashMap<>();
+//        Map<String, FindShortestRoutes.Route> routeDeA = new HashMap<>();
+//        Map<String, FindShortestRoutes.Route> routeDeB = new HashMap<>();
+//        Map<String, FindShortestRoutes.Route> routeDeC = new HashMap<>();
+//        Map<String, FindShortestRoutes.Route> routeDeD = new HashMap<>();
+//        Map<String, FindShortestRoutes.Route> routeDeE = new HashMap<>();
+//        Map<String, FindShortestRoutes.Route> routeDeF = new HashMap<>();
+//        Map<String, FindShortestRoutes.Route> routeDeG = new HashMap<>();
 
-        routeDeDepot.put("Depot", new FindShortestRoutes.Route("Depot","A", Collections.singletonList(segments.get("Depot").get("A")),2));
-        routeDeDepot.put("Depot", new FindShortestRoutes.Route("Depot", "D", Collections.singletonList(segments.get("Depot").get("D")), 20));
-        routeDeDepot.put("Depot", new FindShortestRoutes.Route("Depot", "G", Collections.singletonList(segments.get("Depot").get("G")), 1));
-        routeDeA.put("A", new FindShortestRoutes.Route("A", "Depot", Collections.singletonList(segments.get("A").get("Depot")), 2));
-        routeDeA.put("A", new FindShortestRoutes.Route("A", "B", Collections.singletonList(segments.get("A").get("B")), 3));
-        routeDeA.put("A", new FindShortestRoutes.Route("A", "F", Collections.singletonList(segments.get("A").get("F")), 8));
-        routeDeB.put("B", new FindShortestRoutes.Route("B", "A", Collections.singletonList(segments.get("B").get("A")), 3));
-        routeDeB.put("B", new FindShortestRoutes.Route("B", "C", Collections.singletonList(segments.get("B").get("C")), 3));
-        routeDeB.put("B", new FindShortestRoutes.Route("B", "E", Collections.singletonList(segments.get("B").get("E")), 8));
-        routeDeB.put("B", new FindShortestRoutes.Route("B", "G", Collections.singletonList(segments.get("B").get("G")), 8));
-        routeDeC.put("C", new FindShortestRoutes.Route("C", "B", Collections.singletonList(segments.get("C").get("B")), 3));
-        routeDeC.put("C", new FindShortestRoutes.Route("C", "D", Collections.singletonList(segments.get("C").get("D")), 2));
-        routeDeC.put("C", new FindShortestRoutes.Route("C", "E", Collections.singletonList(segments.get("C").get("E")), 5));
-        routeDeC.put("C", new FindShortestRoutes.Route("C", "F", Collections.singletonList(segments.get("C").get("F")), 8));
-        routeDeD.put("D", new FindShortestRoutes.Route("D", "Depot", Collections.singletonList(segments.get("D").get("Depot")), 20));
-        routeDeD.put("D", new FindShortestRoutes.Route("D", "C", Collections.singletonList(segments.get("D").get("C")), 2));
-        routeDeD.put("D", new FindShortestRoutes.Route("D", "E", Collections.singletonList(segments.get("D").get("E")), 2));
-        routeDeE.put("E", new FindShortestRoutes.Route("E", "B", Collections.singletonList(segments.get("E").get("B")), 8));
-        routeDeE.put("E", new FindShortestRoutes.Route("E", "C", Collections.singletonList(segments.get("E").get("C")), 5));
-        routeDeE.put("E", new FindShortestRoutes.Route("E", "D", Collections.singletonList(segments.get("E").get("D")), 2));
-        routeDeE.put("E", new FindShortestRoutes.Route("E", "F", Collections.singletonList(segments.get("E").get("F")), 3));
-        routeDeF.put("F", new FindShortestRoutes.Route("F", "A", Collections.singletonList(segments.get("F").get("A")), 8));
-        routeDeF.put("F", new FindShortestRoutes.Route("F", "C", Collections.singletonList(segments.get("F").get("C")), 8));
-        routeDeF.put("F", new FindShortestRoutes.Route("F", "E", Collections.singletonList(segments.get("F").get("E")), 3));
-        routeDeF.put("F", new FindShortestRoutes.Route("F", "G", Collections.singletonList(segments.get("F").get("G")), 3));
-        routeDeG.put("G", new FindShortestRoutes.Route("G", "Depot", Collections.singletonList(segments.get("G").get("Depot")), 1));
-        routeDeG.put("G", new FindShortestRoutes.Route("G", "B", Collections.singletonList(segments.get("G").get("B")), 8));
-        routeDeG.put("G", new FindShortestRoutes.Route("G", "F", Collections.singletonList(segments.get("G").get("F")), 3));
+        Dijkstra dd = new Dijkstra();
+        PassagePoint[] pps = new PassagePoint[]{
+                P1,
+                P2,
+                P3,
+                P4,
+                D1,
+                D2,
+                D3,
+                D4,
+                depot,
+        };
+        Map<String, Map<String, FindShortestRoutes.Route>> routes = dd.solve(gM,pps);
 
 
+//        routeDeDepot.put("A", new FindShortestRoutes.Route("Depot","A", Collections.singletonList(segments.get("Depot").get("A")),2));
+//        routeDeDepot.put("D", new FindShortestRoutes.Route("Depot", "D", Collections.singletonList(segments.get("Depot").get("D")), 20));
+//        routeDeDepot.put("G", new FindShortestRoutes.Route("Depot", "G", Collections.singletonList(segments.get("Depot").get("G")), 1));
+//        routeDeA.put("Depot", new FindShortestRoutes.Route("A", "Depot", Collections.singletonList(segments.get("A").get("Depot")), 2));
+//        routeDeA.put("B", new FindShortestRoutes.Route("A", "B", Collections.singletonList(segments.get("A").get("B")), 3));
+//        routeDeA.put("F", new FindShortestRoutes.Route("A", "F", Collections.singletonList(segments.get("A").get("F")), 8));
+//        routeDeB.put("A", new FindShortestRoutes.Route("B", "A", Collections.singletonList(segments.get("B").get("A")), 3));
+//        routeDeB.put("C", new FindShortestRoutes.Route("B", "C", Collections.singletonList(segments.get("B").get("C")), 3));
+//        routeDeB.put("E", new FindShortestRoutes.Route("B", "E", Collections.singletonList(segments.get("B").get("E")), 8));
+//        routeDeB.put("G", new FindShortestRoutes.Route("B", "G", Collections.singletonList(segments.get("B").get("G")), 8));
+//        routeDeC.put("B", new FindShortestRoutes.Route("C", "B", Collections.singletonList(segments.get("C").get("B")), 3));
+//        routeDeC.put("D", new FindShortestRoutes.Route("C", "D", Collections.singletonList(segments.get("C").get("D")), 2));
+//        routeDeC.put("E", new FindShortestRoutes.Route("C", "E", Collections.singletonList(segments.get("C").get("E")), 5));
+//        routeDeC.put("F", new FindShortestRoutes.Route("C", "F", Collections.singletonList(segments.get("C").get("F")), 8));
+//        routeDeD.put("Depot", new FindShortestRoutes.Route("D", "Depot", Collections.singletonList(segments.get("D").get("Depot")), 20));
+//        routeDeD.put("C", new FindShortestRoutes.Route("D", "C", Collections.singletonList(segments.get("D").get("C")), 2));
+//        routeDeD.put("E", new FindShortestRoutes.Route("D", "E", Collections.singletonList(segments.get("D").get("E")), 2));
+//        routeDeE.put("B", new FindShortestRoutes.Route("E", "B", Collections.singletonList(segments.get("E").get("B")), 8));
+//        routeDeE.put("C", new FindShortestRoutes.Route("E", "C", Collections.singletonList(segments.get("E").get("C")), 5));
+//        routeDeE.put("D", new FindShortestRoutes.Route("E", "D", Collections.singletonList(segments.get("E").get("D")), 2));
+//        routeDeE.put("F", new FindShortestRoutes.Route("E", "F", Collections.singletonList(segments.get("E").get("F")), 3));
+//        routeDeF.put("A", new FindShortestRoutes.Route("F", "A", Collections.singletonList(segments.get("F").get("A")), 8));
+//        routeDeF.put("C", new FindShortestRoutes.Route("F", "C", Collections.singletonList(segments.get("F").get("C")), 8));
+//        routeDeF.put("E", new FindShortestRoutes.Route("F", "E", Collections.singletonList(segments.get("F").get("E")), 3));
+//        routeDeF.put("G", new FindShortestRoutes.Route("F", "G", Collections.singletonList(segments.get("F").get("G")), 3));
+//        routeDeG.put("Depot", new FindShortestRoutes.Route("G", "Depot", Collections.singletonList(segments.get("G").get("Depot")), 1));
+//        routeDeG.put("B", new FindShortestRoutes.Route("G", "B", Collections.singletonList(segments.get("G").get("B")), 8));
+//        routeDeG.put("F", new FindShortestRoutes.Route("G", "F", Collections.singletonList(segments.get("G").get("F")), 3));
 
 
 
 
-        routes.put("Depot", routeDeDepot);
-        routes.put("A", routeDeA);
-        routes.put("B", routeDeB);
-        routes.put("C", routeDeC);
-        routes.put("D", routeDeD);
-        routes.put("E", routeDeE);
-        routes.put("F", routeDeF);
-        routes.put("G", routeDeG);
+
+
+//        routes.put("Depot", routeDeDepot);
+//        routes.put("A", routeDeA);
+//        routes.put("B", routeDeB);
+//        routes.put("C", routeDeC);
+//        routes.put("D", routeDeD);
+//        routes.put("E", routeDeE);
+//        routes.put("F", routeDeF);
+//        routes.put("G", routeDeG);
 
 
         OptimalHamiltonianCircuit algoHamilton = new OptimalHamiltonianCircuit();
@@ -162,8 +177,14 @@ public class OptimalHamiltonianCircuitTest {
         assertEquals(P3, result.get(3));
         assertEquals(P4, result.get(4));
         assertEquals(D3, result.get(5));
-        assertEquals(D1, result.get(6));
-        assertEquals(D2, result.get(7));
+        if(result.get(6).equals(D2)){
+            assertEquals(D2, result.get(6));
+            assertEquals(D1, result.get(7));
+        } else{
+            assertEquals(D1, result.get(6));
+            assertEquals(D2, result.get(7));
+        }
+        assertEquals(D1, result.get(7));
         assertEquals(D4, result.get(8));
         assertEquals(depot, result.get(9));
     }

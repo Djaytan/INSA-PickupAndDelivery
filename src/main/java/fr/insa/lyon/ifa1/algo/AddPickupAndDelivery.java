dc489080f876980063c8cecc4c56b894920afbcb
cyrelouyea <alerycserrania@protonmail.com>
2021-03-26 15:17:12 +0100

    AddPickupAndDelivery

---
+++ src/main/java/fr/insa/lyon/ifa1/algo/AddPickupAndDelivery.java
@@ -0,0 +1,15 @@
+package fr.insa.lyon.ifa1.algo;
+
+import fr.insa.lyon.ifa1.models.map.GeoMap;
+import fr.insa.lyon.ifa1.models.request.PassagePoint;
+import fr.insa.lyon.ifa1.models.request.Request;
+
+import java.util.List;
+import java.util.Map;
+
+public interface AddPickupAndDelivery {
+
+    void solve(GeoMap gm, Request r, List<PassagePoint> circuit, Map<String, Map<String, FindShortestRoutes.Route>> routes);
+
+
+}
